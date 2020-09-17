9556d63f24140d4412ba1ab37a38a1508bdc8623
Praveen <pkchmits@gmail.com>
2020-09-17 09:38:50 +0530

    Done with fragment lifecycle calls with Timble Library Logs

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -14,16 +14,18 @@
  * limitations under the License.
  */
 
- package com.example.android.navigation
+package com.example.android.navigation
 
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
-import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
+import timber.log.Timber
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -40,24 +42,65 @@ class TitleFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
-                R.layout.fragment_title,container,false)
+                R.layout.fragment_title, container, false)
 
-        binding.playButton.setOnClickListener { view : View ->
+        binding.playButton.setOnClickListener { view: View ->
             view.findNavController().navigate(TitleFragmentDirections.actionTitleFragmentToGameFragment())
         }
         setHasOptionsMenu(true)
+        Timber.i("OnCreateView Called")
         return binding.root
     }
 
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.options_menu,menu)
+        inflater.inflate(R.menu.options_menu, menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
                 || super.onOptionsItemSelected(item)
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Timber.i( "onAttach called")
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.i( "onCreate called")
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Timber.i( "onViewCreated called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i( "onStart called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Timber.i( "onResume called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Timber.i( "onPause called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Timber.i( "onStop called")
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Timber.i( "onDestroyView called")
+    }
+    override fun onDetach() {
+        super.onDetach()
+        Timber.i( "onDetach called")
     }
 
 }
